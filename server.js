58bba33f93524930cd4c67abbf737ae6d6a2e501
AstraNova1 <VariousKid4@gmail.com>
2026-01-02 21:36:33 +0700

    Update server.js

--- server.js
+++ server.js
@@ -7,37 +7,21 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-// 🔥 WAJIB: biar index.html bisa dibuka
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "index.html"));
-});
-
-// Simpan user online
-let users = {};
-
-// Socket.IO
-io.on("connection", socket => {
-  console.log("User connected:", socket.id);
-
-  socket.on("join", ({ userId, name }) => {
-    users[socket.id] = { userId, name };
-    io.emit("users", Object.values(users));
-  });
-
-  socket.on("message", msg => {
-    io.emit("message", msg);
-  });
-
-  socket.on("disconnect", () => {
-    delete users[socket.id];
-    io.emit("users", Object.values(users));
-    console.log("User disconnected:", socket.id);
-  });
-});
-
-// 🔥 INI BAGIAN PORT YANG WAJIB UNTUK DEPLOY
+// PORT WAJIB PAKAI ENV (buat deploy)
 const PORT = process.env.PORT || 3000;
 
-server.listen(PORT, () => {
-  console.log("✅ Server running on port", PORT);
+// serve file static (HTML, CSS, JS)
+app.use(express.static(path.join(__dirname, "public")));
+
+// test route biar ga "Cannot GET /"
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
+
+// socket.io
+io.on("connection", (socket) => {
+  console.log("User connected:", socket.id);
+
+  socket.on("chat message", (msg) => {
+    io.emit("chat message", msg);
+  });
